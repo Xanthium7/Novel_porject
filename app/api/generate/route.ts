e8300a51bce28a186be13178448d7e2afc457e4d
Akshhay <=>
2023-09-17 09:16:29 +0530

    impemented AI intergration

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,9 +1,9 @@
-// import OpenAI from "openai";
-// import { OpenAIStream, StreamingTextResponse } from "ai";
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
-// const openai = new OpenAI({
-//     apiKey: process.env.OPENAI_API_KEY,
-// });
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 
 export async function POST(req: Request): Promise<Response>{
@@ -13,27 +13,27 @@ export async function POST(req: Request): Promise<Response>{
     let prompt = JSON.parse(promt).prompt
 
 
-    // const chatCompletion = await openai.chat.completions.create({
-    //     messages: [
-    //         {
-    //           role: "system",
-    //           content:
-    //             "You are an AI writing assistant that continues existing text based on context from prior text. " +
-    //             "Give more weight/priority to the later characters than the beginning ones. " +
-    //             "Limit your response to no more than 100 characters, but make sure to construct complete sentences.",
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [
+            {
+              role: "system",
+              content:
+                "You are an AI writing assistant that continues existing text based on context from prior text. " +
+                "Give more weight/priority to the later characters than the beginning ones. " +
+                "Limit your response to no more than 100 characters, but make sure to construct complete sentences.",
               
-    //         },
-    //         {
-    //           role: "user",
-    //           content: prompt,
-    //         },
-    //       ],
-    //     model: "gpt-3.5-turbo",
-    // });
+            },
+            {
+              role: "user",
+              content: prompt,
+            },
+          ],
+        model: "gpt-3.5-turbo",
+    });
 
-    // console.log(chatCompletion)
+    console.log(chatCompletion.choices[0].message.content)
 
-    return new Response(" Ai system is functional but not accessible due to lack of API key, please view the git hub repo code for further information")
+    return new Response(chatCompletion.choices[0].message.content )
     
 }
 
