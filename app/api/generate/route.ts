4054b43daff2ebed7e4aef76da0b414a6c32d064
Akshhay <=>
2023-09-17 10:01:12 +0530

    impreleneted website updates for the newly added AI feature

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,12 +1,12 @@
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+// import OpenAI from "openai";
+// import { OpenAIStream, StreamingTextResponse } from "ai";
 
 
 
 
-const openai = new OpenAI({
-    apiKey: "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",
-});
+// const openai = new OpenAI({
+//     apiKey: "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",
+// });
 
 
 export async function POST(req: Request): Promise<Response>{
@@ -16,28 +16,28 @@ export async function POST(req: Request): Promise<Response>{
     let prompt = JSON.parse(promt).prompt
 
 
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [
-            {
-              role: "system",
-              content:
-                "You are an AI writing assistant that continues existing text based on context from prior text. " +
-                "Give more weight/priority to the later characters than the beginning ones. " +
-                "Limit your response to no more than 100 characters, but make sure to construct complete sentences.",
+    // const chatCompletion = await openai.chat.completions.create({
+    //     messages: [
+    //         {
+    //           role: "system",
+    //           content:
+    //             "You are an AI writing assistant that continues existing text based on context from prior text. " +
+    //             "Give more weight/priority to the later characters than the beginning ones. " +
+    //             "Limit your response to no more than 100 characters, but make sure to construct complete sentences.",
               
-            },
-            {
-              role: "user",
-              content: prompt,
-            },
-          ],
-        model: "gpt-3.5-turbo",
-    });
+    //         },
+    //         {
+    //           role: "user",
+    //           content: prompt,
+    //         },
+    //       ],
+    //     model: "gpt-3.5-turbo",
+    // });
 
-    console.log(chatCompletion.choices[0].message.content)
+    // console.log(chatCompletion.choices[0].message.content)
 
     return(
-        new Response(": "+chatCompletion.choices[0].message.content )
+        new Response(": "+"CLone our github repo and add ur own api key to continue")
         ) 
     
 }
